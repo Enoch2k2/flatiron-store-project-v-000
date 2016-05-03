e8511d70b16ae48e6012dfa2f0fa71e67f287285
Enoch Griffith <enoch2k2@hotmail.com>
2016-05-02 20:34:15 -0500

    Done.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,16 @@
 class CartsController < ApplicationController
 
+  def show
+    @cart = Cart.find(params[:id])
+  end
+
+  def checkout
+    @cart = Cart.find(params[:id])
+    @cart.line_items.each do |line_item|
+      Item.find(line_item.item_id).decrement(:inventory, line_item.quantity).save
+    end
+    current_user.current_cart = nil
+    redirect_to cart_path(params[:id])
+  end
+
 end
